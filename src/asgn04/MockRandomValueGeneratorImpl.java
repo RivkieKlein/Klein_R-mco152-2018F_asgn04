d11f3bb0221e5404e3ff1fee4371cd8e50d10f4f
RivkieKlein <43257799+RivkieKlein@users.noreply.github.com>
2018-11-05 14:23:48 -0500

    created mock random generator

---
+++ src/asgn04/MockRandomValueGeneratorImpl.java
@@ -0,0 +1,17 @@
+package asgn04;
+
+public class MockRandomValueGeneratorImpl implements RandomValueGenerator {
+	private int notRlyRand;
+
+	@Override
+	public int getRandomValue() {
+		return notRlyRand;
+	}
+	
+	public void setRandomValue(int num) {
+		notRlyRand=num;
+	}
+	
+	
+
+}
